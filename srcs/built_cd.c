0dc0ab19e7b86e1b28a3fdd40c70d2f1518db322
Jeremy BOCKTOR <jbocktor@made-f0Ar7s8.clusters.42paris.fr>
2024-05-17 14:59:55 +0200

    add built_in to branch v1

---
+++ srcs/built_cd.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   built_cd.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jbocktor <jbocktor@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/17 13:08:44 by jbocktor          #+#    #+#             */
+/*   Updated: 2024/05/17 13:34:23 by jbocktor         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+/*
+int		i;
+char	*path;
+
+i = 0;
+while (cd)
+	i++;
+if (i > 1)
+{
+	// ft_printf("bash: %s: too many arguments\n", cd[0]);
+	return (0);
+}
+path = cd[1];
+chdir(path);
+*/
+
+int	built_cd(char **cd)
+{
+	(void)cd;
+	return (0);
+}
---
+++ srcs/run_bi_minishell.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   run_bi_minishell.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jbocktor <jbocktor@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/17 13:09:29 by jbocktor          #+#    #+#             */
+/*   Updated: 2024/05/17 13:09:30 by jbocktor         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+int	check_builtin_type_and_run_bi(char ***envp, int *envp_size, t_cmd *bi)
+{
+	*envp = *envp;
+	*envp_size = *envp_size;
+	if (bi && bi->cmd && bi->cmd[0] != '\0')
+	{
+		if (ft_strncmp("echo", bi->cmd, 4) == 0)
+			return (built_echo(bi->args));
+		if (ft_strncmp("cd", bi->cmd, 2) == 0)
+			return (built_cd(bi->args));
+		if (ft_strncmp("pwd", bi->cmd, 3) == 0)
+			return (built_pwd(bi->args));
+		if (ft_strncmp("export", bi->cmd, 6) == 0)
+			return (built_export(envp, envp_size, bi->args));
+		if (ft_strncmp("unset", bi->cmd, 5) == 0)
+			return (built_unset(envp, envp_size, bi->args));
+		if (ft_strncmp("env", bi->cmd, 3) == 0)
+			return (built_env(*envp, bi->args));
+		if (ft_strncmp("exit", bi->cmd, 4) == 0)
+			return (built_exit(bi->args));
+	}
+	else if (!(bi && bi->cmd))
+		display_err_with_prefix("(null)", " command not found\n");
+	else
+		display_err_with_prefix(bi->cmd, " command not found\n");
+	return (127);
+}
