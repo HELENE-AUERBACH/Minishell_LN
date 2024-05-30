ed985bbfcbecb9d58643f36b705601db1bdf72ee
Jeremy BOCKTOR <jbocktor@bess-f2r4s14.clusters.42paris.fr>
2024-05-30 16:05:26 +0200

    improve exit

--- srcs/built_exit.c
+++ srcs/built_exit.c
@@ -6,27 +6,40 @@
 /*   By: jbocktor <jbocktor@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/17 13:09:03 by jbocktor          #+#    #+#             */
-/*   Updated: 2024/05/30 13:23:28 by hauerbac         ###   ########.fr       */
+/*   Updated: 2024/05/30 14:40:20 by jbocktor         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int	built_exit(t_data *d, char **exit_args)
+static int	too_many_args(char **args)
+{
+	int	i;
+
+	i = 0;
+	while (args[i])
+		i++;
+	if (i > 2)
+		return (1);
+	return (0);
+}
+
+int	built_exit(t_data *d, char **args)
 {
 	char		*endptr;
 	long long	value;
 
-	if (!exit_args[1])
+	if (!args[1])
 		value = g_exit_status;
+	else if (too_many_args(args))
+		return (display_err_with_prefix(args[0], " too many arguments\n"), 1);
 	else
 	{
-		value = ft_strtoll(exit_args[1], &endptr);
-		if (endptr == exit_args[1] || *endptr != '\0'
-			|| value < LLONG_MIN || value > LLONG_MAX)
+		value = ft_strtoll(args[1], &endptr);
+		if (endptr == args[1] || *endptr != '\0' || value < LLONG_MIN
+			|| value > LLONG_MAX)
 		{
-			display_err_with_prefix(exit_args[1], \
-						" numeric argument required\n");
+			display_err_with_prefix(args[1], " numeric argument required\n");
 			value = 2;
 		}
 	}
@@ -36,5 +49,5 @@ int	built_exit(t_data *d, char **exit_args)
 	if (is_in_interactive_mode())
 		rl_clear_history();
 	free_data(d);
-	exit (value % 256);
+	exit(value % 256);
 }
